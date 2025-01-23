9bff1c50e400290acbf8e71249facecb658496bf
Kirill Lalayants <lalayantskirill@gmail.com>
2025-01-23 20:31:42 +0000

    new weights

--- test_tpu.py
+++ test_tpu.py
@@ -4,20 +4,20 @@ from PIL import Image
 import time
 
 model = YOLO(
-    f"yolov8n_256_sim_best_full_integer_quant_edgetpu.tflite",
+    f"wights/yolov8n_640_full_integer_quant_edgetpu.tflite",
     task="detect",
 )
 
-img_file = np.asarray(Image.open('bus.jpg').resize((256, 256)))
+img_file = np.asarray(Image.open('bus.jpg').resize((640, 640)))
 print(img_file.shape)
-res = model.predict(img_file, imgsz=256, device="tpu:1", verbose=False)
+res = model.predict(img_file, imgsz=640, device="tpu:1", verbose=False)
 frames = 10
 
 print(f"Eval {frames} frames on YOLO EdgeTpu")
 start_time = time.time()
 i = 0
 while i < frames:
-    res = model.predict(img_file, imgsz=256, verbose=False)
+    res = model.predict(img_file, imgsz=640, verbose=False)
     i += 1
 print(f"Average FPS: {frames / (time.time() - start_time)}")
 res = model.predict(img_file, verbose=True)
