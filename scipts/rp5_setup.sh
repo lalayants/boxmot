f1b23ad03086db3c20a1a837afbad4394a5cf688
Kirill Lalayants <lalayantskirill@gmail.com>
2025-01-23 22:24:22 +0300

    new script

--- scipts/rp5_setup.sh
+++ scipts/rp5_setup.sh
@@ -3,7 +3,6 @@ sudo apt install ./drivers_tpu/libedgetpu1-std_16.0tf2.17.0-1.bookworm_arm64.deb
 # sudo echo "dtparam=pciex1" >> /boot/firmware/config.txt
 # sudo echo "kernel=kernel8.img" >> /boot/firmware/config.txt
 # sudo echo "dtoverlay=pineboards-hat-ai" >> /boot/firmware/config.txt
-# On Raspbian needed to be added manually into /boot/firmware/config.txt
 echo '[all]' | sudo tee -a /boot/firmware/config.txt
 echo 'dtparam=pciex1_gen=3' | sudo tee -a /boot/firmware/config.txt
 echo '# Enable the PCIe External connector.' | sudo tee -a /boot/firmware/config.txt
@@ -30,5 +29,8 @@ sudo depmod -a
 # sudo sh get-docker.sh && dockerd-rootless-setuptool.sh install
 
 python -m venv ~/venv && source ~/venv/bin/activate && pip install tensorflow tflite_runtime
+
 # Reboot
 sudo reboot
+
+#  curl https://gist.githubusercontent.com/dataslayermedia/714ec5a9601249d9ee754919dea49c7e/raw/32d21f73bd1ebb33854c2b059e94abe7767c3d7e/coral-ai-pcie-edge-tpu-raspberrypi-5-setup | sh
