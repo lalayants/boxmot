e0ae490a7a585b63d9796b0bcc1527b3d96622fe
Kirill Lalayants <lalayantskirill@gmail.com>
2025-01-28 21:52:08 +0000

    done

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 export DISPLAY=:0
-poetry run python tracking/track.py \
+poetry run python tracking/track.py --imgsz 480 \
     --yolo-model yolov8n \
     --tracking-method imprassoc\
     --show --show-trajectories \
-    --source 0 \
+    --source video.mp4 \
     # --source 'https://ultralytics.com/images/bus640.jpg'\
 
     # --show --show-trajectories
--- tpu_run.sh
+++ tpu_run.sh
@@ -1,6 +1,6 @@
 export DISPLAY=:0
-poetry run python tracking/track.py --imgsz 640 \
-    --yolo-model myweights/yolov8n_640_full_integer_quant_edgetpu.tflite --device tpu:0 \
+poetry run python tracking/track.py --imgsz 480 \
+    --yolo-model myweights/yolov8n_480_full_integer_quant_edgetpu.tflite --device tpu:0 \
     --tracking-method imprassoc\
     --show --show-trajectories \
     --source 0 \
