cf380ec534b440ccc3d4240c195a9f3d42659c11
Kirill Lalayants <lalayantskirill@gmail.com>
2025-01-29 13:52:40 +0000

    bytetrack

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 export DISPLAY=:0
 poetry run python tracking/track.py --imgsz 480 \
     --yolo-model yolov8n \
-    --tracking-method imprassoc\
+    --tracking-method bytetrack\
     --show --show-trajectories \
     --source video.mp4 \
     # --source 'https://ultralytics.com/images/bus640.jpg'\
--- tpu_run.sh
+++ tpu_run.sh
@@ -1,9 +1,9 @@
 export DISPLAY=:0
 poetry run python tracking/track.py --imgsz 480 \
     --yolo-model myweights/yolov8n_480_full_integer_quant_edgetpu.tflite --device tpu:0 \
-    --tracking-method imprassoc\
+    --tracking-method bytetrack \
     --show --show-trajectories \
-    --source 0 \
+    --source video.mp4 \
     # --source 'https://ultralytics.com/images/bus640.jpg'\
 
     # --show --show-trajectories
