ed0792aab7a54b10e2ef282b7db270fa42275789
Kirill Lalayants <lalayantskirill@gmail.com>
2025-02-22 18:12:04 +0300

    metrics done

--- benchmark_scripts/run.sh
+++ benchmark_scripts/run.sh
@@ -4,12 +4,17 @@ set -e
 echo "Starting benchmark..."
 cd "$(dirname "$0")/.."
 
-
+# reid_models=("osnet_x1_0_dukemtmcreid.pt" "osnet_x1_0_market1501.pt" "osnet_x1_0_msmt17.pt")
 # reid_models=("resnet50_msmt17.pt" "resnet50_fc512_msmt17.pt" "clip_market1501.pt" "lmbn_n_cuhk03_d.pt")
 # yolo_weights=("yolov8n.pt" "yolov8s.pt" "yolov8m.pt" "yolov8l.pt" "yolov8x.pt")
-trackers=("ocsort" "bytetrack" "botsort" "deepocsort" "imprassoc" "strongsort")
-yolo_weights=("yolov8n_runs_512p_450epoches.pt" "yolov8s_runs_512p_450epoches.pt" "yolov8m_runs_512p_450epoches.pt" "yolov8l_runs_512p_450epoches.pt" "yolov8x_runs_512p_450epoches.pt")
-reid_models=("osnet_x1_0_dukemtmcreid.pt" "osnet_x0_75_dukemtmcreid.pt" "osnet_x0_5_dukemtmcreid.pt" "osnet_x0_25_dukemtmcreid.pt")
+trackers=("strongsort" "ocsort" "bytetrack" "botsort" "deepocsort" "imprassoc")
+yolo_weights=("yolov8n_runs_512p_450epoches.pt" "yolov8s_runs_512p_450epoches.pt" "yolov8m_runs_512p_450epoches.pt" "yolov8l_runs_512p_450epoches.pt" "yolov8x_runs_512p_450epoches.pt"
+            "yolov8n_runs_320p_600epoches.pt" "yolov8s_runs_320p_600epoches.pt" "yolov8m_runs_320p_600epoches.pt" "yolov8l_runs_320p_600epoches.pt" "yolov8x_runs_320p_600epoches.pt"
+            )
+reid_models=("osnet_x1_0_market1501.pt" "osnet_x0_75_market1501.pt" "osnet_x0_5_market1501.pt" "osnet_x0_25_market1501.pt" "clip_market1501.pt" "lmbn_n_market.pt" "clip_market1501.pt" "osnet_ibn_x1_0_msmt17.pt" "osnet_ain_x1_0_msmt17.pt")
+
+
+
 
 # Set the dataset directory.
 DATASET_DIR="tracking/val_utils/data/MOT17-50/train"
