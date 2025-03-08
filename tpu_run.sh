0258894434c4b138a6c6e2007527e33fe4122cdb
Kirill Lalayants <lalayantskirill@gmail.com>
2025-03-08 23:16:25 +0300

    fps test ready

--- tpu_run.sh
+++ tpu_run.sh
@@ -2,6 +2,7 @@ export DISPLAY=:0
 poetry run python tracking/track.py --imgsz 320 \
     --yolo-model ../tpu_weights/v8/320/yolov8n_full_integer_quant_edgetpu.tflite  \
     --device tpu:0 \
-    --tracking-method bytetrack \
+    --tracking-method strongsort \
+    --reid-model osnet_x1_0_market1501.pt \
     --classes 0 \
     --source video.mp4 
