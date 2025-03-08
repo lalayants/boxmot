60c2bebf5b19555fa17dcaa9a863f53175aa690d
Kirill Lalayants <lalayantskirill@gmail.com>
2025-03-08 18:17:53 +0000

    clean tpu run

--- tpu_run.sh
+++ tpu_run.sh
@@ -2,8 +2,5 @@ export DISPLAY=:0
 poetry run python tracking/track.py --imgsz 480 \
     --yolo-model myweights/yolov8n_480_full_integer_quant_edgetpu.tflite --device tpu:0 \
     --tracking-method bytetrack \
-    --show --show-trajectories \
-    --source video.mp4 \
-    # --source 'https://ultralytics.com/images/bus640.jpg'\
-
-    # --show --show-trajectories
+    --classes 0 \
+    --source video.mp4 
