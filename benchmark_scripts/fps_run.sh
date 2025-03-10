670ad77be0c4339ae05ff89a8f0e804dee2e3c00
Kirill Lalayants <lalayantskirill@gmail.com>
2025-03-10 15:46:27 +0300

    fps metrics ready

--- benchmark_scripts/fps_run.sh
+++ benchmark_scripts/fps_run.sh
@@ -5,7 +5,7 @@ export DISPLAY=:0
 
 # Define arrays (adjust as needed)
 yolo_weights=("yolov8n.pt" "yolov8s.pt" "yolov8m.pt" "yolov8l.pt" "yolov8x.pt")
-reid_models=("osnet_x1_0_market1501.pt" "osnet_x0_75_market1501.pt" "osnet_x0_5_market1501.pt" "osnet_x0_25_market1501.pt" "clip_market1501.pt" "lmbn_n_market.pt" "clip_market1501.pt" "osnet_ibn_x1_0_msmt17.pt" "osnet_ain_x1_0_msmt17.pt")
+reid_models=("osnet_x1_0_market1501.pt" "osnet_x0_75_market1501.pt" "osnet_x0_5_market1501.pt" "osnet_x0_25_market1501.pt" "lmbn_n_market.pt" "clip_market1501.pt" "osnet_ibn_x1_0_msmt17.pt" "osnet_ain_x1_0_msmt17.pt")
 trackers=("strongsort" "ocsort" "bytetrack" "botsort" "deepocsort" "imprassoc")
 # yolo_weights=("yolov8n.pt")
 
--- benchmark_scripts/metrics_run.sh
+++ benchmark_scripts/metrics_run.sh
@@ -4,17 +4,11 @@ set -e
 echo "Starting benchmark..."
 cd "$(dirname "$0")/.."
 
-# reid_models=("osnet_x1_0_dukemtmcreid.pt" "osnet_x1_0_market1501.pt" "osnet_x1_0_msmt17.pt")
-# reid_models=("resnet50_msmt17.pt" "resnet50_fc512_msmt17.pt" "clip_market1501.pt" "lmbn_n_cuhk03_d.pt")
-# yolo_weights=("yolov8n.pt" "yolov8s.pt" "yolov8m.pt" "yolov8l.pt" "yolov8x.pt")
-# trackers=("strongsort" "ocsort" "bytetrack" "botsort" "deepocsort" "imprassoc")
-# yolo_weights=("yolov8n_runs_512p_450epoches.pt" "yolov8s_runs_512p_450epoches.pt" "yolov8m_runs_512p_450epoches.pt" "yolov8l_runs_512p_450epoches.pt" "yolov8x_runs_512p_450epoches.pt"
-#             "yolov8n_runs_320p_600epoches.pt" "yolov8s_runs_320p_600epoches.pt" "yolov8m_runs_320p_600epoches.pt" "yolov8l_runs_320p_600epoches.pt" "yolov8x_runs_320p_600epoches.pt"
-#             )
-# reid_models=("osnet_x1_0_market1501.pt" "osnet_x0_75_market1501.pt" "osnet_x0_5_market1501.pt" "osnet_x0_25_market1501.pt" "clip_market1501.pt" "lmbn_n_market.pt" "clip_market1501.pt" "osnet_ibn_x1_0_msmt17.pt" "osnet_ain_x1_0_msmt17.pt")
-trackers=("bytetrack")
-reid_models=("osnet_x1_0_msmt17.pt")
-yolo_weights=("yolov8n_runs_320p_600epoches.pt")
+trackers=("strongsort" "ocsort" "bytetrack" "botsort" "deepocsort" "imprassoc")
+yolo_weights=("yolov8n_runs_512p_450epoches.pt" "yolov8s_runs_512p_450epoches.pt" "yolov8m_runs_512p_450epoches.pt" "yolov8l_runs_512p_450epoches.pt" "yolov8x_runs_512p_450epoches.pt"
+            "yolov8n_runs_320p_600epoches.pt" "yolov8s_runs_320p_600epoches.pt" "yolov8m_runs_320p_600epoches.pt" "yolov8l_runs_320p_600epoches.pt" "yolov8x_runs_320p_600epoches.pt"
+            )
+reid_models=("osnet_x1_0_market1501.pt" "osnet_x0_75_market1501.pt" "osnet_x0_5_market1501.pt" "osnet_x0_25_market1501.pt" "clip_market1501.pt" "lmbn_n_market.pt" "osnet_ibn_x1_0_msmt17.pt" "osnet_ain_x1_0_msmt17.pt")
 
 
 
